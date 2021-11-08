73717091c3e5658b3cdc92be908e9c4f890867bc
James Kong <jkongie@gmail.com>
2021-11-08 13:11:27 +0800

    Add generated types

---
+++ operator_ui/src/types/generated/possibleTypes.ts
@@ -0,0 +1,103 @@
+
+      export type PossibleTypesResultData = {
+  "possibleTypes": {
+    "BridgePayload": [
+      "Bridge",
+      "NotFoundError"
+    ],
+    "CreateBridgePayload": [
+      "CreateBridgeSuccess"
+    ],
+    "CreateCSAKeyPayload": [
+      "CSAKeyExistsError",
+      "CreateCSAKeySuccess"
+    ],
+    "CreateFeedsManagerPayload": [
+      "CreateFeedsManagerSuccess",
+      "InputErrors",
+      "NotFoundError",
+      "SingleFeedsManagerError"
+    ],
+    "DeleteOCRKeyBundlePayload": [
+      "DeleteOCRKeyBundleSuccess",
+      "NotFoundError"
+    ],
+    "Error": [
+      "CSAKeyExistsError",
+      "InputError",
+      "NotFoundError",
+      "SingleFeedsManagerError"
+    ],
+    "FeaturesPayload": [
+      "Features"
+    ],
+    "FeedsManagerPayload": [
+      "FeedsManager",
+      "NotFoundError"
+    ],
+    "PaginatedPayload": [
+      "BridgesPayload"
+    ],
+    "UpdateBridgePayload": [
+      "NotFoundError",
+      "UpdateBridgeSuccess"
+    ],
+    "UpdateFeedsManagerPayload": [
+      "InputErrors",
+      "NotFoundError",
+      "UpdateFeedsManagerSuccess"
+    ]
+  }
+};
+      const result: PossibleTypesResultData = {
+  "possibleTypes": {
+    "BridgePayload": [
+      "Bridge",
+      "NotFoundError"
+    ],
+    "CreateBridgePayload": [
+      "CreateBridgeSuccess"
+    ],
+    "CreateCSAKeyPayload": [
+      "CSAKeyExistsError",
+      "CreateCSAKeySuccess"
+    ],
+    "CreateFeedsManagerPayload": [
+      "CreateFeedsManagerSuccess",
+      "InputErrors",
+      "NotFoundError",
+      "SingleFeedsManagerError"
+    ],
+    "DeleteOCRKeyBundlePayload": [
+      "DeleteOCRKeyBundleSuccess",
+      "NotFoundError"
+    ],
+    "Error": [
+      "CSAKeyExistsError",
+      "InputError",
+      "NotFoundError",
+      "SingleFeedsManagerError"
+    ],
+    "FeaturesPayload": [
+      "Features"
+    ],
+    "FeedsManagerPayload": [
+      "FeedsManager",
+      "NotFoundError"
+    ],
+    "PaginatedPayload": [
+      "BridgesPayload"
+    ],
+    "UpdateBridgePayload": [
+      "NotFoundError",
+      "UpdateBridgeSuccess"
+    ],
+    "UpdateFeedsManagerPayload": [
+      "InputErrors",
+      "NotFoundError",
+      "UpdateFeedsManagerSuccess"
+    ]
+  }
+};
+      export default result;
+    
